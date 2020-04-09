936cc25bdc22570dda92a7cc89fcf8c696069861
falam113 <46723221+falam113@users.noreply.github.com>
2020-04-08 18:04:30 -0600

    Update config-users.inc.php

--- config-users.inc.php
+++ config-users.inc.php
@@ -1,7 +1,7 @@
 <?php
-define('DBHOST', 'localhost');
-define('DBNAME', 'users');
-define('DBUSER', 'root');
-define('DBPASS', '');
+define('DBHOST', 'l3855uft9zao23e2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com');
+define('DBNAME', 'hbw0jq35zjeu6vv3');
+define('DBUSER', 'pf98ajljtab333ay');
+define('DBPASS', 'ufiuk6zrn7wkdtgb');
 define('DBCONNSTRING',"mysql:host=" . DBHOST . ";dbname=" . DBNAME . ";charset=utf8mb4;");
 ?>
